1574836963e599bc10a8b2b27d83174993129ec2
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2022-03-23 01:51:57 -0700

    canvas-video-downloader add start/end index and retry ability

--- python/canvas-video-downloader.py
+++ python/canvas-video-downloader.py
@@ -5,11 +5,13 @@ import requests
 import os
 
 LECTURE_LIST = [
-    # ["folder_name", "link_part_1", "link_part_2"],
+    # ["folder_name", "link_part_1", "link_part_2", "optional start index", "optional end index"],
 ]
 # the `link_part_1` and `link_part_2` parts are just the two different parts of the link that are separated by a "counter"
 # normally, streams are done by downloading a bunch of ".ts" files that are consecutively numbered
 # customize as necessary
+
+MAX_RETRIES = 10 # maximum number of retries for each .ts file (useful for shaky connections)
 
 cwd = os.getcwd()
 print(cwd)
@@ -36,27 +38,30 @@ def get_url(number, folder, url1, url2):
 
     return True
 
-
-OPTIONAL_IDX_END = False # set to False if unused
-
 for lecture in LECTURE_LIST:
     LECTURE_TITLE = lecture[0]
     idx = 1
+    idxEnd = -1
+    if len(lecture) >= 4: idx = lecture[3]
+    if len(lecture) >= 5: idxEnd = lecture[4]
 
     if not os.path.exists(LECTURE_TITLE):
         print(f"{LECTURE_TITLE} folder is not found, creating the folder.")
         os.makedirs(LECTURE_TITLE)
 
+    retries = 0
+
     print(f"## STARTING DOWNLOAD FOR: '{LECTURE_TITLE}'")
 
-    while(True):
+    while(retries != MAX_RETRIES):
         result = get_url(idx, LECTURE_TITLE, lecture[1], lecture[2])
 
-        if(result == False or idx == OPTIONAL_IDX_END):
+        if(result == False or (idxEnd and idx == idxEnd)):
             print("FOUND BROKEN LINK")
-            break
-
-        idx += 1
+            retries += 1
+        else:
+            retries = 0
+            idx += 1
 
     print(f"** COMPLETED DOWNLOADING STREAM FOR: '{LECTURE_TITLE}'")
     # os.system(f"cat {LECTURE_TITLE}/*.ts >> {LECTURE_TITLE}.ts")
