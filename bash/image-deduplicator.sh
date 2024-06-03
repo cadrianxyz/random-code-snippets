7bfe408d79bad3b3dd9fcfa192feb89f71e4d7f5
Clarence Adrian <44561376+ad2969@users.noreply.github.com>
2024-06-02 22:11:08 -0700

    feat(bash/image-deduplicator): better filter and use reverse sort instead

--- bash/image-deduplicator.sh
+++ bash/image-deduplicator.sh
@@ -1,8 +1,8 @@
 # example is for *.HEIC file types
 prev_base=""
 
-find . -type f -name "* (*).HEIC" | sort | while read file; do
-    base_name=$(echo "$file" | sed -E 's/ \([0-9]+\)\.HEIC$//')
+find . -type f -name "*.HEIC" | sort -r | while read file; do
+    base_name=$(echo "$file" | sed -E 's/( \([0-9]+\))?\.HEIC$//')
    
     if [[ "$base_name" != "$prev_base" ]]; then
         prev_base="$base_name"
