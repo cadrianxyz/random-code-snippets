6f6c4a55d819ec62f5ab22fb58e90409b177c68d
Clarence Adrian <44561376+ad2969@users.noreply.github.com>
2024-06-02 22:00:55 -0700

    feat(bash/image-deduplicator): push simple bash script

---
+++ bash/image-deduplicator.sh
@@ -0,0 +1,13 @@
+# example is for *.HEIC file types
+prev_base=""
+
+find . -type f -name "* (*).HEIC" | sort | while read file; do
+    base_name=$(echo "$file" | sed -E 's/ \([0-9]+\)\.HEIC$//')
+   
+    if [[ "$base_name" != "$prev_base" ]]; then
+        prev_base="$base_name"
+    else;
+        echo "Deleting $file"
+        rm "$file"
+    fi
+done
